d528fb006ade709a3bfd33f5fcd5b3d9fbaf0bdb
Jonathan Mousserion <quezako35@gmail.com>
2019-02-08 19:53:05 +0100

    Sanitize details.

--- src/app/details/details.module.ts
+++ src/app/details/details.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { DetailsPage } from './details.page';
+import { SanitizeHtmlPipe } from '../sanitize-html.pipe';
 
 const routes: Routes = [
   {
@@ -21,6 +22,9 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [DetailsPage]
+  declarations: [
+    DetailsPage,
+    SanitizeHtmlPipe
+  ]
 })
 export class DetailsPageModule {}
